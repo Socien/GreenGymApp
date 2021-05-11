81113c38470ae1d7e1e0cb10ffd9537a856f672b
Socien <yyumiwing@ajou.ac.kr>
2021-05-12 02:50:29 +0900

    greengym

--- app/src/main/java/com/example/greengym/Report.java
+++ app/src/main/java/com/example/greengym/Report.java
@@ -2,15 +2,26 @@ package com.example.greengym;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.InputStream;
 
 public class Report extends AppCompatActivity {
 
-    private Button next;
+    private EditText content;
+    private ImageView image;
+    private Button add, next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +32,38 @@ public class Report extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setTitle("운동기구 고장 신고");
 
+        content = (EditText) findViewById((R.id.report_content));
+        image = (ImageView) findViewById((R.id.image));
+        add = (Button) findViewById((R.id.add_image));
         next = (Button) findViewById(R.id.report_next);
+        next.setEnabled(false); //버튼 비활성화
+
+        //신고 내용
+        content.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() > 9){
+                    next.setEnabled(true);
+                    next.setBackgroundColor(Color.parseColor("#1EAE0E"));
+                }
+            }
+        });
+
+        //사진 추가 버튼
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               Intent intent = new Intent(Intent.ACTION_PICK);
+               intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+               startActivityForResult(intent, 1);
+            }
+        });
 
         //다음버튼
         next.setOnClickListener(new View.OnClickListener(){
@@ -31,5 +73,24 @@ public class Report extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //사진 추가 관련
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                try{
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    image.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/greengym/Report_Location.java
+++ app/src/main/java/com/example/greengym/Report_Location.java
@@ -1,12 +1,27 @@
 package com.example.greengym;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Report_Location extends AppCompatActivity {
+
+
+    private EditText name, phone;
+    private Spinner city, gu;
+    private Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +31,73 @@ public class Report_Location extends AppCompatActivity {
         //액션바 제목
         ActionBar actionbar = getSupportActionBar();
         actionbar.setTitle("운동기구 고장 신고");
+
+        name = (EditText) findViewById((R.id.name));
+        phone = (EditText) findViewById((R.id.phone));
+        city = (Spinner) findViewById(R.id.city_spinner);
+        gu = (Spinner) findViewById(R.id.gu_spinner);
+        ok = (Button) findViewById(R.id.report_ok);
+        ok.setEnabled(false); //버튼 비활성화
+
+        //이름 입력
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() > 1){
+                    ok.setEnabled(true);
+                    ok.setBackgroundColor(Color.parseColor("#1EAE0E"));
+                }
+            }
+        });
+
+        //전화번호 입력
+        phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() > 10){
+                    ok.setEnabled(true);
+                    ok.setBackgroundColor(Color.parseColor("#1EAE0E"));
+                }
+            }
+        });
+
+        //시/도 선택
+        ArrayAdapter cityAdapter = ArrayAdapter.createFromResource(this, R.array.cityArray, android.R.layout.simple_spinner_dropdown_item);
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        city.setAdapter(cityAdapter);
+
+        //구 선택
+        ArrayAdapter guAdapter = ArrayAdapter.createFromResource(this, R.array.guArray, android.R.layout.simple_spinner_dropdown_item);
+        guAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gu.setAdapter(guAdapter);
+
+        //다음버튼
+        ok.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder log = new AlertDialog.Builder(Report_Location.this);
+                log.setMessage("신고가 접수되었습니다.");
+                log.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                log.show();
+            }
+        });
     }
 }
