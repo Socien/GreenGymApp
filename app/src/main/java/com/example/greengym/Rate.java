6aebe0df1186be5ffaa0ac8d0898ac69f5b14c20
Socien <yyumiwing@ajou.ac.kr>
2021-05-20 00:22:28 +0900

    05.20

--- app/src/main/java/com/example/greengym/Rate.java
+++ app/src/main/java/com/example/greengym/Rate.java
@@ -2,22 +2,27 @@ package com.example.greengym;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Rate extends AppCompatActivity {
-    private Button startBtn, stopBtn, statistic;
-    private TextView timeTextView, max_recordTextView, recordTextView;
-    private Thread timeThread = null;
-    private Boolean isRunning = true;
+
+    private Button start, stop, statistic;
+    private TextView time, max_record, record;
+
+    long startTime;
+    long sumTime = 0;
+    private int i = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +33,18 @@ public class Rate extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setTitle("운동량 측정");
 
-        startBtn = (Button) findViewById(R.id.start);
-        stopBtn = (Button) findViewById(R.id.stop);
+        start = (Button) findViewById(R.id.start);
+        stop = (Button) findViewById(R.id.stop);
         statistic = (Button) findViewById(R.id.statistic);
-        timeTextView = (TextView) findViewById(R.id.time);
-        max_recordTextView = (TextView) findViewById(R.id.max_record);
-        recordTextView = (TextView) findViewById(R.id.record);
+        time = (TextView) findViewById(R.id.time);
+        max_record = (TextView) findViewById(R.id.max_record);
+        record = (TextView) findViewById(R.id.record);
 
-        //시작 버튼
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeThread = new Thread(new timeThread());
-                timeThread.start();
-            }
-        });
+        //버튼 비활성화
+        stop.setEnabled(false);
 
-        //정지 버튼
-        stopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recordTextView.setText(recordTextView.getText() + timeTextView.getText().toString() + "\n");
-
-                timeThread.interrupt();
-            }
-        });
+        //기록 스크롤
+        record.setMovementMethod(new ScrollingMovementMethod());
 
         //운동량 통계
         statistic.setOnClickListener(new View.OnClickListener(){
@@ -62,48 +54,57 @@ public class Rate extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //시작 버튼
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = SystemClock.elapsedRealtime();
+                Timer.sendEmptyMessage(0);
+
+                //버튼
+                start.setEnabled(false);
+                start.setBackgroundColor(Color.parseColor("#70A6A1A1"));
+                stop.setEnabled(true);
+                stop.setBackgroundColor(Color.parseColor("#1EAE0E"));
+            }
+        });
+
+        //정지 버튼
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //기록
+                String result1 = String.format("%d  |  " + time.getText().toString() + "\n" + record.getText() , i);
+                record.setText(result1);
+                i++;
+
+                //멈춤
+                Timer.removeMessages(0);
+                long nowTime = SystemClock.elapsedRealtime();
+                sumTime += (nowTime - startTime);
+                String result2 = String.format("%02d : %02d : %02d", (sumTime / 1000) / 60, (sumTime / 1000) % 60, (sumTime % 1000) / 10);
+                max_record.setText(result2);
+                time.setText("00 : 00 : 00");
+
+                //버튼
+                stop.setEnabled(false);
+                stop.setBackgroundColor(Color.parseColor("#70A6A1A1"));
+                start.setEnabled(true);
+                start.setBackgroundColor(Color.parseColor("#1EAE0E"));
+            }
+        });
     }
 
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
+    @SuppressLint({"DefaultLocale", "HandlerLeak"})
+    Handler Timer = new Handler(){
         @Override
-        public void handleMessage(Message msg) {
-            int ss = msg.arg1 % 60;
-            int s = (msg.arg1 / 60) % 60;
-            int m = (msg.arg1 / 3600);
-
-        @SuppressLint("DefaultLocale") String result = String.format("%02d : %02d : %02d", m, s, ss);
-        timeTextView.setText(result);
+        public void handleMessage(Message msg){
+            long nowTime = SystemClock.elapsedRealtime();
+            long outTime = nowTime - startTime;
+             String result = String.format("%02d : %02d : %02d", outTime / 1000 / 60, (outTime / 1000) % 60, (outTime % 1000) / 10);
+            time.setText(result);
+            Timer.sendEmptyMessage(0);
         }
     };
-
-    public class timeThread implements Runnable {
-        @Override
-        public void run() {
-            int i = 0;
-
-            while (true) {
-                while (isRunning) {
-                    Message msg = new Message();
-                    msg.arg1 = i++;
-                    handler.sendMessage(msg);
-
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                timeTextView.setText("00 : 00 : 00");
-                            }
-                        });
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-
 }
